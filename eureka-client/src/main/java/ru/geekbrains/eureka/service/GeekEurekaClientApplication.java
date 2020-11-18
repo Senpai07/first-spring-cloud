77b2e618ff9eaf6a40397fd48822f4d4ab0ca95c
Senpai <senpai@mail.ru>
2020-11-18 22:39:25 +0300

    Initial commit

---
+++ eureka-client/src/main/java/ru/geekbrains/eureka/service/GeekEurekaClientApplication.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekEurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeekEurekaClientApplication.class, args);
+    }
+}
