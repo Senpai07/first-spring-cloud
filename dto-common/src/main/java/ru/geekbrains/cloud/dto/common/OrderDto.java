b7901015697cde2c76867b84cb5bce8cb7b001e3
Senpai <senpai@mail.ru>
2020-11-20 00:06:07 +0300

    + try to install dto-common

--- dto-common/src/main/java/ru/geekbrains/cloud/dto/common/OrderDto.java
+++ dto-common/src/main/java/ru/geekbrains/cloud/dto/common/OrderDto.java
@@ -5,4 +5,36 @@ public class OrderDto {
     private int price;
     private String address;
     private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- dto-common/src/main/java/ru/geekbrains/cloud/dto/common/ProductDto.java
+++ dto-common/src/main/java/ru/geekbrains/cloud/dto/common/ProductDto.java
@@ -1,14 +1,32 @@
 package ru.geekbrains.cloud.dto.common;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class ProductDto {
     private Long id;
     private String title;
     private int price;
-    private String categoryTitle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
 }
