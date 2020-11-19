2b8652101d83b4a2d63e98580ec008ad533eb406
Senpai <senpai@mail.ru>
2020-11-20 00:31:06 +0300

    + fill product-service

--- product-service/src/main/java/ru/geekbrains/product/service/ProductService.java
+++ product-service/src/main/java/ru/geekbrains/product/service/ProductService.java
@@ -1,18 +1,17 @@
 package ru.geekbrains.product.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public Page<Product> findAll(Specification<Product> spec, int page, int size) {
-        return productRepository.findAll(spec, PageRequest.of(page, size));
+    public List<Product> findAll() {
+        return productRepository.findAll();
     }
 
 }
